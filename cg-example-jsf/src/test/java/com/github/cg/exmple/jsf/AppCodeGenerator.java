ddc7acf55353d06633dc47063ca7cab0ddddb3ed
Pablo Filetti Moreira <pablo.filetti@gmail.com>
2015-09-24 10:33:38 -0300

    Ajuste

--- cg-example-jsf/src/test/java/com/github/cg/exmple/jsf/AppCodeGenerator.java
+++ cg-example-jsf/src/test/java/com/github/cg/exmple/jsf/AppCodeGenerator.java
@@ -1,15 +1,12 @@
 package com.github.cg.exmple.jsf;
 
 import com.github.cg.CodeGenerator;
-import com.github.cg.example.core.model.Model;
 
 public class AppCodeGenerator {
 
 	public static void main(String[] args) throws Exception {
 		
 		CodeGenerator cg = new CodeGenerator();
-//		cg.start();
-		cg.executeTargetByName("frmEntity.xhtml", Model.class);
-		System.exit(0);
+		cg.start();
 	}
 }
